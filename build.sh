81ea73086698ad40a9131969a9d8869d2ae4f124
administrator <tzhang2024@gmail.com>
2026-08-04 15:13:04 +0800

    Seed demo activity during configured deploys

--- build.sh
+++ build.sh
@@ -4,3 +4,9 @@ set -o errexit
 python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [[ -n "${DEMO_READER_PASSWORD:-}" && -n "${DEMO_ADMIN_PASSWORD:-}" ]]; then
+    python manage.py seed_demo_content \
+        --reader-password "$DEMO_READER_PASSWORD" \
+        --admin-password "$DEMO_ADMIN_PASSWORD"
+fi
